9f606c425f93b7b6c4ace316aeae25c3713da0a4
mehmetalioyur <77691403+mehmetalioyur@users.noreply.github.com>
2022-01-30 21:55:29 +0300

    Constants.kt added

--- app/src/main/java/com/mehmetalioyur/findmovieapp/util/Constants.kt
+++ app/src/main/java/com/mehmetalioyur/findmovieapp/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.mehmetalioyur.findmovieapp.util
 
 object Constants {
-    const val API_KEY = "Join your api key here."
+    const val API_KEY = ""Replace with your API Key"
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w500"
 }
